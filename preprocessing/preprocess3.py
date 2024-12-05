8320bff314b5fddf557bce0047ee5cbb99b0a697
CombustibleToast <34487106+CombustibleToast@users.noreply.github.com>
2024-12-05 12:29:06 -0500

    Update preprocessing files for submission

--- preprocessing/preprocess3.py
+++ preprocessing/preprocess3.py
@@ -1,10 +1,1132 @@
+#from google.colab import drive # For use on google colab
 import pandas as pd
 import os
 import numpy as np
 from time import time
-import json
+import re
 
-hero_mapping = json.load(open("heroes_parsed.json"))
+#role mapping: {'Carry': 0, 'Escape': 1, 'Nuker': 2, 'Initiator': 3, 'Durable': 4, 'Disabler': 5, 'Support': 6, 'Pusher': 7}
+# Fold the line below this one
+hero_mapping = {
+    "antimage": {
+        "id": 1,
+        "roles": [
+            0,
+            1,
+            2
+        ]
+    },
+    "axe": {
+        "id": 2,
+        "roles": [
+            3,
+            4,
+            5,
+            0
+        ]
+    },
+    "bane": {
+        "id": 3,
+        "roles": [
+            6,
+            5,
+            2,
+            4
+        ]
+    },
+    "bloodseeker": {
+        "id": 4,
+        "roles": [
+            0,
+            5,
+            2,
+            3
+        ]
+    },
+    "crystalmaiden": {
+        "id": 5,
+        "roles": [
+            6,
+            5,
+            2
+        ]
+    },
+    "drowranger": {
+        "id": 6,
+        "roles": [
+            0,
+            5,
+            7
+        ]
+    },
+    "earthshaker": {
+        "id": 7,
+        "roles": [
+            6,
+            3,
+            5,
+            2
+        ]
+    },
+    "juggernaut": {
+        "id": 8,
+        "roles": [
+            0,
+            7,
+            1
+        ]
+    },
+    "mirana": {
+        "id": 9,
+        "roles": [
+            0,
+            6,
+            1,
+            2,
+            5
+        ]
+    },
+    "morphling": {
+        "id": 10,
+        "roles": [
+            0,
+            1,
+            4,
+            2,
+            5
+        ]
+    },
+    "nevermore": {
+        "id": 11,
+        "roles": [
+            0,
+            2
+        ]
+    },
+    "phantomlancer": {
+        "id": 12,
+        "roles": [
+            0,
+            1,
+            7,
+            2
+        ]
+    },
+    "puck": {
+        "id": 13,
+        "roles": [
+            3,
+            5,
+            1,
+            2
+        ]
+    },
+    "pudge": {
+        "id": 14,
+        "roles": [
+            5,
+            3,
+            4,
+            2
+        ]
+    },
+    "razor": {
+        "id": 15,
+        "roles": [
+            0,
+            4,
+            2,
+            7
+        ]
+    },
+    "sandking": {
+        "id": 16,
+        "roles": [
+            3,
+            5,
+            6,
+            2,
+            1
+        ]
+    },
+    "stormspirit": {
+        "id": 17,
+        "roles": [
+            0,
+            1,
+            2,
+            3,
+            5
+        ]
+    },
+    "sven": {
+        "id": 18,
+        "roles": [
+            0,
+            5,
+            3,
+            4,
+            2
+        ]
+    },
+    "tiny": {
+        "id": 19,
+        "roles": [
+            0,
+            2,
+            7,
+            3,
+            4,
+            5
+        ]
+    },
+    "vengefulspirit": {
+        "id": 20,
+        "roles": [
+            6,
+            3,
+            5,
+            2,
+            1
+        ]
+    },
+    "windrunner": {
+        "id": 21,
+        "roles": [
+            0,
+            6,
+            5,
+            1,
+            2
+        ]
+    },
+    "zuus": {
+        "id": 22,
+        "roles": [
+            2,
+            0
+        ]
+    },
+    "kunkka": {
+        "id": 23,
+        "roles": [
+            0,
+            6,
+            5,
+            3,
+            4,
+            2
+        ]
+    },
+    "lina": {
+        "id": 25,
+        "roles": [
+            6,
+            0,
+            2,
+            5
+        ]
+    },
+    "lion": {
+        "id": 26,
+        "roles": [
+            6,
+            5,
+            2,
+            3
+        ]
+    },
+    "shadowshaman": {
+        "id": 27,
+        "roles": [
+            6,
+            7,
+            5,
+            2,
+            3
+        ]
+    },
+    "slardar": {
+        "id": 28,
+        "roles": [
+            0,
+            4,
+            3,
+            5,
+            1
+        ]
+    },
+    "tidehunter": {
+        "id": 29,
+        "roles": [
+            3,
+            4,
+            5,
+            2,
+            0
+        ]
+    },
+    "witchdoctor": {
+        "id": 30,
+        "roles": [
+            6,
+            2,
+            5
+        ]
+    },
+    "lich": {
+        "id": 31,
+        "roles": [
+            6,
+            2
+        ]
+    },
+    "riki": {
+        "id": 32,
+        "roles": [
+            0,
+            1,
+            5
+        ]
+    },
+    "enigma": {
+        "id": 33,
+        "roles": [
+            5,
+            3,
+            7
+        ]
+    },
+    "tinker": {
+        "id": 34,
+        "roles": [
+            0,
+            2,
+            7
+        ]
+    },
+    "sniper": {
+        "id": 35,
+        "roles": [
+            0,
+            2
+        ]
+    },
+    "necrolyte": {
+        "id": 36,
+        "roles": [
+            0,
+            2,
+            4,
+            5
+        ]
+    },
+    "warlock": {
+        "id": 37,
+        "roles": [
+            6,
+            3,
+            5
+        ]
+    },
+    "beastmaster": {
+        "id": 38,
+        "roles": [
+            3,
+            5,
+            4,
+            2
+        ]
+    },
+    "queenofpain": {
+        "id": 39,
+        "roles": [
+            0,
+            2,
+            1
+        ]
+    },
+    "venomancer": {
+        "id": 40,
+        "roles": [
+            6,
+            2,
+            3,
+            7,
+            5
+        ]
+    },
+    "facelessvoid": {
+        "id": 41,
+        "roles": [
+            0,
+            3,
+            5,
+            1,
+            4
+        ]
+    },
+    "skeletonking": {
+        "id": 42,
+        "roles": [
+            0,
+            6,
+            4,
+            5,
+            3
+        ]
+    },
+    "deathprophet": {
+        "id": 43,
+        "roles": [
+            0,
+            7,
+            2,
+            5
+        ]
+    },
+    "phantomassassin": {
+        "id": 44,
+        "roles": [
+            0,
+            1
+        ]
+    },
+    "pugna": {
+        "id": 45,
+        "roles": [
+            2,
+            7
+        ]
+    },
+    "templarassassin": {
+        "id": 46,
+        "roles": [
+            0,
+            1
+        ]
+    },
+    "viper": {
+        "id": 47,
+        "roles": [
+            0,
+            4,
+            3,
+            5
+        ]
+    },
+    "luna": {
+        "id": 48,
+        "roles": [
+            0,
+            2,
+            7
+        ]
+    },
+    "dragonknight": {
+        "id": 49,
+        "roles": [
+            0,
+            7,
+            4,
+            5,
+            3,
+            2
+        ]
+    },
+    "dazzle": {
+        "id": 50,
+        "roles": [
+            6,
+            2,
+            5
+        ]
+    },
+    "rattletrap": {
+        "id": 51,
+        "roles": [
+            3,
+            5,
+            4,
+            2
+        ]
+    },
+    "leshrac": {
+        "id": 52,
+        "roles": [
+            0,
+            6,
+            2,
+            7,
+            5
+        ]
+    },
+    "furion": {
+        "id": 53,
+        "roles": [
+            0,
+            7,
+            1,
+            2
+        ]
+    },
+    "lifestealer": {
+        "id": 54,
+        "roles": [
+            0,
+            4,
+            1,
+            5
+        ]
+    },
+    "darkseer": {
+        "id": 55,
+        "roles": [
+            3,
+            1,
+            5
+        ]
+    },
+    "clinkz": {
+        "id": 56,
+        "roles": [
+            0,
+            1,
+            7
+        ]
+    },
+    "omniknight": {
+        "id": 57,
+        "roles": [
+            6,
+            4,
+            2
+        ]
+    },
+    "enchantress": {
+        "id": 58,
+        "roles": [
+            6,
+            7,
+            4,
+            5
+        ]
+    },
+    "huskar": {
+        "id": 59,
+        "roles": [
+            0,
+            4,
+            3
+        ]
+    },
+    "nightstalker": {
+        "id": 60,
+        "roles": [
+            0,
+            3,
+            4,
+            5,
+            2
+        ]
+    },
+    "broodmother": {
+        "id": 61,
+        "roles": [
+            0,
+            7,
+            1,
+            2
+        ]
+    },
+    "bountyhunter": {
+        "id": 62,
+        "roles": [
+            1,
+            2
+        ]
+    },
+    "weaver": {
+        "id": 63,
+        "roles": [
+            0,
+            1
+        ]
+    },
+    "jakiro": {
+        "id": 64,
+        "roles": [
+            6,
+            2,
+            7,
+            5
+        ]
+    },
+    "batrider": {
+        "id": 65,
+        "roles": [
+            3,
+            5,
+            1
+        ]
+    },
+    "chen": {
+        "id": 66,
+        "roles": [
+            6,
+            7
+        ]
+    },
+    "spectre": {
+        "id": 67,
+        "roles": [
+            0,
+            4,
+            1
+        ]
+    },
+    "ancientapparition": {
+        "id": 68,
+        "roles": [
+            6,
+            5,
+            2
+        ]
+    },
+    "doombringer": {
+        "id": 69,
+        "roles": [
+            0,
+            5,
+            3,
+            4,
+            2
+        ]
+    },
+    "ursa": {
+        "id": 70,
+        "roles": [
+            0,
+            4,
+            5
+        ]
+    },
+    "spiritbreaker": {
+        "id": 71,
+        "roles": [
+            0,
+            3,
+            5,
+            4,
+            1
+        ]
+    },
+    "gyrocopter": {
+        "id": 72,
+        "roles": [
+            0,
+            2,
+            5
+        ]
+    },
+    "alchemist": {
+        "id": 73,
+        "roles": [
+            0,
+            6,
+            4,
+            5,
+            3,
+            2
+        ]
+    },
+    "invoker": {
+        "id": 74,
+        "roles": [
+            0,
+            2,
+            5,
+            1,
+            7
+        ]
+    },
+    "silencer": {
+        "id": 75,
+        "roles": [
+            0,
+            6,
+            5,
+            3,
+            2
+        ]
+    },
+    "obsidiandestroyer": {
+        "id": 76,
+        "roles": [
+            0,
+            2,
+            5
+        ]
+    },
+    "lycan": {
+        "id": 77,
+        "roles": [
+            0,
+            7,
+            4,
+            1
+        ]
+    },
+    "brewmaster": {
+        "id": 78,
+        "roles": [
+            0,
+            3,
+            4,
+            5,
+            2
+        ]
+    },
+    "shadowdemon": {
+        "id": 79,
+        "roles": [
+            6,
+            5,
+            3,
+            2
+        ]
+    },
+    "lonedruid": {
+        "id": 80,
+        "roles": [
+            0,
+            7,
+            4
+        ]
+    },
+    "chaosknight": {
+        "id": 81,
+        "roles": [
+            0,
+            5,
+            4,
+            7,
+            3
+        ]
+    },
+    "meepo": {
+        "id": 82,
+        "roles": [
+            0,
+            1,
+            2,
+            5,
+            3,
+            7
+        ]
+    },
+    "treant": {
+        "id": 83,
+        "roles": [
+            6,
+            3,
+            4,
+            5,
+            1
+        ]
+    },
+    "ogremagi": {
+        "id": 84,
+        "roles": [
+            6,
+            2,
+            5,
+            4,
+            3
+        ]
+    },
+    "undying": {
+        "id": 85,
+        "roles": [
+            6,
+            4,
+            5,
+            2
+        ]
+    },
+    "rubick": {
+        "id": 86,
+        "roles": [
+            6,
+            5,
+            2
+        ]
+    },
+    "disruptor": {
+        "id": 87,
+        "roles": [
+            6,
+            5,
+            2,
+            3
+        ]
+    },
+    "nyxassassin": {
+        "id": 88,
+        "roles": [
+            5,
+            2,
+            3,
+            1
+        ]
+    },
+    "nagasiren": {
+        "id": 89,
+        "roles": [
+            0,
+            6,
+            7,
+            5,
+            3,
+            1
+        ]
+    },
+    "keeperofthelight": {
+        "id": 90,
+        "roles": [
+            6,
+            2,
+            5
+        ]
+    },
+    "wisp": {
+        "id": 91,
+        "roles": [
+            6,
+            1,
+            2
+        ]
+    },
+    "visage": {
+        "id": 92,
+        "roles": [
+            6,
+            2,
+            4,
+            5,
+            7
+        ]
+    },
+    "slark": {
+        "id": 93,
+        "roles": [
+            0,
+            1,
+            5,
+            2
+        ]
+    },
+    "medusa": {
+        "id": 94,
+        "roles": [
+            0,
+            5,
+            4
+        ]
+    },
+    "trollwarlord": {
+        "id": 95,
+        "roles": [
+            0,
+            7,
+            5,
+            4
+        ]
+    },
+    "centaur": {
+        "id": 96,
+        "roles": [
+            4,
+            3,
+            5,
+            2,
+            1
+        ]
+    },
+    "magnataur": {
+        "id": 97,
+        "roles": [
+            3,
+            5,
+            2,
+            1
+        ]
+    },
+    "shredder": {
+        "id": 98,
+        "roles": [
+            2,
+            4,
+            1
+        ]
+    },
+    "bristleback": {
+        "id": 99,
+        "roles": [
+            0,
+            4,
+            3,
+            2
+        ]
+    },
+    "tusk": {
+        "id": 100,
+        "roles": [
+            3,
+            5,
+            2
+        ]
+    },
+    "skywrathmage": {
+        "id": 101,
+        "roles": [
+            6,
+            2,
+            5
+        ]
+    },
+    "abaddon": {
+        "id": 102,
+        "roles": [
+            6,
+            0,
+            4
+        ]
+    },
+    "eldertitan": {
+        "id": 103,
+        "roles": [
+            3,
+            5,
+            2,
+            4
+        ]
+    },
+    "legioncommander": {
+        "id": 104,
+        "roles": [
+            0,
+            5,
+            3,
+            4,
+            2
+        ]
+    },
+    "techies": {
+        "id": 105,
+        "roles": [
+            2,
+            5
+        ]
+    },
+    "emberspirit": {
+        "id": 106,
+        "roles": [
+            0,
+            1,
+            2,
+            5,
+            3
+        ]
+    },
+    "earthspirit": {
+        "id": 107,
+        "roles": [
+            2,
+            1,
+            5,
+            3,
+            4
+        ]
+    },
+    "abyssalunderlord": {
+        "id": 108,
+        "roles": [
+            6,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    "terrorblade": {
+        "id": 109,
+        "roles": [
+            0,
+            7,
+            2
+        ]
+    },
+    "phoenix": {
+        "id": 110,
+        "roles": [
+            6,
+            2,
+            3,
+            1,
+            5
+        ]
+    },
+    "oracle": {
+        "id": 111,
+        "roles": [
+            6,
+            2,
+            5,
+            1
+        ]
+    },
+    "winterwyvern": {
+        "id": 112,
+        "roles": [
+            6,
+            5,
+            2
+        ]
+    },
+    "arcwarden": {
+        "id": 113,
+        "roles": [
+            0,
+            1,
+            2
+        ]
+    },
+    "monkeyking": {
+        "id": 114,
+        "roles": [
+            0,
+            1,
+            5,
+            3
+        ]
+    },
+    "darkwillow": {
+        "id": 119,
+        "roles": [
+            6,
+            2,
+            5,
+            1
+        ]
+    },
+    "pangolier": {
+        "id": 120,
+        "roles": [
+            0,
+            2,
+            5,
+            4,
+            1,
+            3
+        ]
+    },
+    "grimstroke": {
+        "id": 121,
+        "roles": [
+            6,
+            2,
+            5,
+            1
+        ]
+    },
+    "hoodwink": {
+        "id": 123,
+        "roles": [
+            6,
+            2,
+            1,
+            5
+        ]
+    },
+    "voidspirit": {
+        "id": 126,
+        "roles": [
+            0,
+            1,
+            2,
+            5
+        ]
+    },
+    "snapfire": {
+        "id": 128,
+        "roles": [
+            6,
+            2,
+            5,
+            1
+        ]
+    },
+    "mars": {
+        "id": 129,
+        "roles": [
+            0,
+            3,
+            5,
+            4
+        ]
+    },
+    "ringmaster": {
+        "id": 131,
+        "roles": [
+            6,
+            2,
+            1,
+            5
+        ]
+    },
+    "dawnbreaker": {
+        "id": 135,
+        "roles": [
+            0,
+            4
+        ]
+    },
+    "marci": {
+        "id": 136,
+        "roles": [
+            6,
+            0,
+            3,
+            5,
+            1
+        ]
+    },
+    "primalbeast": {
+        "id": 137,
+        "roles": [
+            3,
+            4,
+            5
+        ]
+    },
+    "muerta": {
+        "id": 138,
+        "roles": [
+            0,
+            2,
+            5
+        ]
+    },
+    "kez": {
+        "id": 145,
+        "roles": [
+            0,
+            1,
+            5
+        ]
+    }
+}
 
 def create_modified_csv(directory, output_subfolder='modified_data'):
   """
@@ -139,7 +1261,7 @@ def create_modified_csv(directory, output_subfolder='modified_data'):
       for hero_col in hero_cols:
         # Apply mapping function to the column
         df[hero_col] = df[hero_col].apply(
-            lambda hero_name: hero_mapping.get((hero_name).split('_')[-1].lower())['id']  # Extract hero name and get ID
+            lambda hero_name: hero_mapping.get((hero_name).replace('CDOTA_Unit_Hero_', '').replace('_', '').lower()).get('id')  # Extract hero name and get ID
         )
       # Save the modified DataFrame to a new CSV
 
@@ -148,4 +1270,3 @@ def create_modified_csv(directory, output_subfolder='modified_data'):
 
       # Benchmark
       print(f"file process of size {os.path.getsize(filepath)/1000000}MB took {time()-process_time_start:.2f} sec")
-      break # REMOVE TO DO ALL FILES!
--- preprocessing/run3.py
+++ preprocessing/run3.py
@@ -1,3 +1,4 @@
+# This file is used to run the preprocessor locally
 import preprocess3
 
 preprocess3.create_modified_csv("./data", "modified_data")
