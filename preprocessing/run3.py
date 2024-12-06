a572bd3a0cea0078315c9213ccd1b5f24880c6ab
samrl <samrlipton@gmail.com>
2024-12-06 17:44:15 -0500

    Added robust README.MD

--- preprocessing/run3.py
+++ preprocessing/run3.py
@@ -1,4 +1,11 @@
 # This file is used to run the preprocessor locally
 import preprocess3
+import argparse
 
-preprocess3.create_modified_csv("./data", "modified_data")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Preprocess Dota 2 match data.")
+    parser.add_argument("input_folder", type=str, help="Path to the input folder containing parsed match data.")
+    parser.add_argument("output_folder", type=str, help="Path to the output folder for processed CSV files.")
+    args = parser.parse_args()
+
+    preprocess3.create_modified_csv(args.input_folder, args.output_folder)
